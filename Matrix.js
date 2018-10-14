53f6d0d9496c9831cab43db8a89bb1679e85af16
sriramdharba <sriramdharba@yahoo.com>
2018-10-14 21:48:42 +0530

    updated files

--- Matrix.js
+++ Matrix.js
@@ -43,25 +43,79 @@ var arr3 = [];
 
 
 
-/*//2D Matrices Addition
+// //2D Matrices Addition
 
-var arr1 = [[0,1],[2,3]];
-var arr2 = [[2,5],[6,8]];
-var arr3 = [[],[]];
+// var arr1 = [[0,1],[2,3]];
+// var arr2 = [[2,5],[6,8]];
+// var arr3 = [[],[]];
 
-	for(var i=0;i<2;i++){
+// 	for(var i=0;i<2;i++){
 
-		for(var j=0;j<2;j++){
+// 		for(var j=0;j<2;j++){
 
-					arr3[i][j]=arr1[i][j]+arr2[i][j];
-			}
-		}
-		console.log(arr3);*/
+// 					arr3[i][j]=arr1[i][j]+arr2[i][j];
+// 			}
+// 		}
+// 		console.log(arr3);
 
-// 3D Matrices Addition
+// // 3D Matrices Addition
 
-	var arr1 = [[0,1,0],[2,3,1],[4,5,1]];
-	var arr2 = [[2,5,1],[6,8,2],[5,4,2]];
+// 	var arr1 = [[0,1,0],[2,3,1],[4,5,1]];
+// 	var arr2 = [[2,5,1],[6,8,2],[5,4,2]];
+
+// // // to delete index values in Array
+
+// var arr1 =[5,3,8,1];
+// var temp;
+// function index(j){
+
+// 	for(var i=0;i<arr1.length-j;i++){
+
+// 		temp=arr1[arr1.length-j];
+// 		arr1[arr1.length-j]=arr1[j];
+// 		arr1[j] = temp;
+
+		
+// 	}
+// 	console.log(arr1[i]);
+// }
+
+// index(1);
 
 
+// // 
+// var arr1 =[5,3,8,1];
+// var temp;
+// function index(j){
 
+// 	for(var i=0;i<arr1.length;i++){
+
+// 		temp=arr1[arr1.length-1];
+// 		arr1[arr1.length-1]=arr1[i];
+// 		arr1[i] = temp;
+// }
+// console.log(arr1[arr1.length-j])
+// }
+
+// index(1)
+
+x=5;
+console.log(x);
+
+
+function deposit(amount) {
+this.balance += amount;
+}
+function withdraw(amount) {
+if (amount <= this.balance) {
+this.balance -= amount;
+}
+if (amount > this.balance) {
+print("Insufficient funds");
+}
+}
+function toString() {
+return "Balance: " + this.balance;
+}
+
+deposit(500)
--- Prime.js
+++ Prime.js
@@ -115,4 +115,4 @@ for(i=0;i<n;i++){
 
 }
 
-Nprime(100	);
+Nprime(100);
