8cde775c3a56b0b4d7f6af62aea5181b4591fabd
sriramdharba <sriramdharba@yahoo.com>
2018-10-15 20:56:39 +0530

    Todays practice

---
+++ for-in.js
@@ -0,0 +1,16 @@
+//objects using for loops
+
+var person ={
+
+	name:"sriram",
+	age:25,
+	niche:"Altruist"	
+// }
+
+// for (var key in person){
+// console.log(key,person[key]);
+}
+console.log(Object.keys(person));
+console.log(Object.values(person))
+
+
