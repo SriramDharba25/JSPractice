4d63cc52813f112b5f50adef2234856545fb9247
sriramdharba <sriramdharba@yahoo.com>
2018-10-16 19:16:51 +0530

    today workings

---
+++ for-in.js
@@ -0,0 +1,29 @@
+// //objects using for loops
+
+// var person ={
+
+// 	name:"sriram",
+// 	age:25,
+// 	niche:"Altruist"	
+// // }
+
+// // for (var key in person){
+// // console.log(key,person[key]);
+// }
+
+// console.log(Object.keys(person));
+// console.log(Object.values(person))
+function ths(a){
+      try {
+         if(a<10){
+         	return a;
+         }
+         break;
+      } 
+      
+      catch ( e ) {
+         return "a should be less than 10"
+         break;
+      }}
+
+      console.log(ths(20));
---
+++ objects.js
@@ -0,0 +1,12 @@
+<html>
+
+
+
+
+let ages =[32,33,16,40]
+ages = ages.every((age)=>{
+	
+	return age >=18;
+	});
+
+console.log(ages);
